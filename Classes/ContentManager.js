86a15d19941529cb4c5575d4176a397b292cbcf4
GooRoo <yagupov.cpp@outlook.com>
2020-01-26 00:20:44 +0400

    Content manager

--- Classes/ContentManager.js
+++ Classes/ContentManager.js
@@ -1,8 +1,13 @@
-export class ContentProcessor {
+/**
+ * обрабатывает контент внутри блока <div>
+ */
+export class ContentManager {
     /**@type{Range}*/
     static range;
 
     static get range_after_caret(){
+        //создать диапазон
+        let range = document.createRange();
         let activeBlock = document.activeElement;
         //позиция каретки в контенте блока
         let startNode = getSelection().anchorNode;
@@ -12,31 +17,22 @@ export class ContentProcessor {
         //если после каретки в блоке есть контент
         if (endNode) {
             let endOffset = endNode.textContent.length;
-            //создать диапазон
-            let range = document.createRange();
             //установить начало диапазона в позиции курсора
             range.setStart(startNode, startOffset);
             //установить конец диапазона в конце контента элемента
             range.setEnd(endNode, endOffset);
-            //скопировать диапазон в переменную
-            console.log(this,'range after caret:',range);
-            return range;
         }
-        return undefined;
+        return range;
     }
 
     static get content_after_caret() {
-        if (this.range_after_caret){
-            return this.range_after_caret.cloneContents();
-        }
-        return undefined;
+        return this.range_after_caret.cloneContents();
     }
 
     static get range_before_caret(){
         let range= document.createRange();
         range.setStart(getSelection().focusNode,0);
         range.setEnd(getSelection().anchorNode,getSelection().anchorOffset);
-        console.log(this,'range before caret:',range);
         return range;
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,4 @@
 import {App} from "../Classes/App.js";
-import {ContentProcessor} from "../Classes/ContentProcessor.js";
 
-
-export const processor=new ContentProcessor();
 export const app = new App();
 document.body.appendChild(app);
